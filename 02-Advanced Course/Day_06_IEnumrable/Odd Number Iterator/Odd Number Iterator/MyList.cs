dbcf9ad48715d405103d3ca4a5466a5a5db8bd7e
abdelrahmanaa121 <abdelrahmanaa121@gmail.com>
2026-04-06 02:54:01 +0200

    6-4-2026

---
+++ 02-Advanced Course/Day_06_IEnumrable/Odd Number Iterator/Odd Number Iterator/MyList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odd_Number_Iterator
+{
+    internal class MyList : IEnumerable<int>
+    {
+        private int[] items;
+        private int count;
+        public MyList(int capacity = 4) //capacity represent default design space req.
+        {
+            items = new int[capacity]; //here u can change the capacity by passing new one
+            count = 0; //represnt the real number of elemnt in Mylist
+        }
+        public void Add(int num)
+        {
+            if (count == items.Length)
+            {
+                Resize();
+            }
+            items[count] = num;
+            count++;
+        }
+        private void Resize()
+        {
+            int newSize = items.Length * 2;
+            int[] newArray = new int[newSize];
+            Array.Copy(items, newArray, items.Length);
+            items = newArray;
+        }
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (items[i] % 2  != 0)
+                {
+                    yield return items[i];
+                }
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ 02-Advanced Course/Day_06_IEnumrable/Odd Number Iterator/Odd Number Iterator/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Odd_Number_Iterator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList list = new MyList(5);
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+
+            Console.WriteLine("Odd numbers in the list:");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
