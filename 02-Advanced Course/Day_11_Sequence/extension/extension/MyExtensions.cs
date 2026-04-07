d2fc417ded830d242edaa9596f607c3d0ad95fe2
abdelrahmanaa121 <abdelrahmanaa121@gmail.com>
2026-04-07 23:51:45 +0200

    Extension

---
+++ 02-Advanced Course/Day_11_Sequence/extension/extension/MyExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace extension
+{
+    internal static class Extensions
+    {
+        public static int GetAge(this DateTime birthdate)
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - birthdate.Year;
+            if (birthdate.Date > today.AddYears(-age))
+            {
+                age--;
+            }
+            return age;
+        }
+        public static double AvgOfEvenNumbers(this List<int> num)
+        {
+            if (num == null || num.Count == 0)
+            {
+                return 0;
+            }
+            int sum = 0;
+            int count = 0;
+            foreach (var n in num)
+            {
+                if ( n % 2 == 0)
+                {
+                    sum += n;
+                    count++;
+                }
+            }
+            if (count == 0)
+            {
+                return 0;
+            }
+            return (double)sum / count;
+        }
+        public static IEnumerable<T> Paginate<T>(this IEnumerable<T> source,int page,int pagesize)
+        {
+            if (source == null) return Enumerable.Empty<T>();
+            if (page < 1) page = 1;
+            return source.Skip((page - 1) * pagesize).Take(pagesize);
+        }
+        public static string ToJson<T>(this T obj)
+        {
+            if (obj == null) return "null";
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            return JsonSerializer.Serialize(obj, options);           
+        }
+    }
+}
---
+++ 02-Advanced Course/Day_11_Sequence/extension/extension/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace extension
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1-extension method for DateTime to calculate age:
+            Console.WriteLine("\nextension method for DateTime to calculate age\n");
+
+            DateTime BD = new DateTime(1999,12,19);
+            Console.WriteLine($"Your Age is {BD.GetAge()}");
+
+            //2-extension method for List<int> that returns the average of even numbers:
+            Console.WriteLine("\nextension method for List<int> that returns the average of even numbers\n");
+
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+            Console.WriteLine($"{{{string.Join(",", numbers)}}}");
+            Console.WriteLine($"\nAverage is {numbers.AvgOfEvenNumbers()}");
+
+            //3- Write an extension method to make pagination for any collection:
+            Console.WriteLine("\nextension method to make pagination for any collection\n");
+
+            List<string> products = new List<string> { "A", "B", "C", "D", "E", "F", "G", "H" };
+            var page2 = products.Paginate(3, 2);
+            IEnumerable<string> p4 = products.Paginate(4, 2);
+            foreach (var item in page2) Console.WriteLine(item);
+            Console.WriteLine("");
+            foreach (var item in p4) Console.WriteLine(item);
+
+            //4-extension method to convert object to JSON:
+            Console.WriteLine("\nextension method to convert object to JSON\n");
+            var user = new { name = "Ali", Age = 23, Job = "Software Engineer" };
+            Console.WriteLine(user.ToJson());
+
+
+        }
+    }
+}
